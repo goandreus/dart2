92f34dfdc51388a18c7850e32837d9f3fd039eac
Andres Chavez <andreshtml5@gmail.com>
2019-07-30 17:26:40 -0500

    update

---
+++ methos.dart
@@ -0,0 +1,57 @@
+class Vehicle
+{  
+  Vehicle()
+  {
+    print("Hi");
+  }
+  
+  void Drive()
+  {
+    print("Drive drive drive");
+  }
+  
+  int _maxSpeed = 60;
+  
+  void set maxSpeed(int speed)
+  {
+    _maxSpeed = speed * 2;
+  }
+  
+  int get maxSpeed
+  {
+    return _maxSpeed;
+  }
+}
+
+class Car extends Vehicle
+{
+  Car()
+  {
+    print("Custom constructor");
+  }
+  
+  @override
+  void Drive()
+  {
+    print("New Drive");
+  }
+  
+  void Hello()
+  {
+    print("Hello, I am a Car");
+  }
+}
+
+void main()
+{  
+ 
+  
+  Car c1 = new Car();
+  
+  c1.Drive();
+  print(c1.maxSpeed);
+  c1.maxSpeed = 10;
+  print(c1.maxSpeed);
+  c1.Hello();
+ 
+}
