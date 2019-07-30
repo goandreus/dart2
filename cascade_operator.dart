a5832b3c68588ae8572ef2c4d087ea14305611ff
Andres Chavez <andreshtml5@gmail.com>
2019-07-30 17:20:25 -0500

    Update cascade_operator.dart

--- cascade_operator.dart
+++ cascade_operator.dart
@@ -35,15 +35,7 @@ class Car extends Vehicle
 
 void main()
 {  
-  /*Vehicle v1 = new Vehicle(100);
-  Vehicle v2 = new Vehicle.empty();
-  
-  v1.Drive();
-  print(v1.maxSpeed);
-  v1.maxSpeed = 89;
-  print(v1.maxSpeed);
-  
-  print(v2.maxSpeed);*/
+ 
   
   Car c3 = new Car();
   c3
